ffc986657fd306c8026c8693f4fa5b0ff9c335b9
deanna-armstrong <152566979+deanna-armstrong@users.noreply.github.com>
2025-06-08 16:49:54 -0400

    notification table update

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Inventory, InventorySchema } from './inventory.schema';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { NotificationsModule } from '../notifications/notifications.module'; // <-- Add this
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Inventory.name, schema: InventorySchema }
     ]),
+    NotificationsModule // <-- This is REQUIRED
   ],
   controllers: [InventoryController],
   providers: [InventoryService],
